b7349353b9635d6e15c4fe82b7f9fac83fe5f5a8
VojinVelimirovic <vojinvelimirovic2002@gmail.com>
2026-01-12 18:26:12 +0100

    Bugfix pushed faulty version of Program.cs

--- TaskIncidentTracker.Api/Program.cs
+++ TaskIncidentTracker.Api/Program.cs
@@ -11,23 +11,17 @@ using TaskIncidentTracker.Api.Common;
 
 var builder = WebApplication.CreateBuilder(args);
 
-// Read connection string - environment variable overrides appsettings
-var connectionString = Environment.GetEnvironmentVariable("ConnectionStrings__DefaultConnection")
-    ?? builder.Configuration.GetConnectionString("DefaultConnection");
-
-// Configure DbContext
 builder.Services.AddDbContext<ApplicationDbContext>(options =>
-    options.UseSqlServer(connectionString));
+    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
 
-// Register services
 builder.Services.AddScoped<IPasswordHasher, PasswordHasher>();
 builder.Services.AddSingleton<IJwtTokenService, JwtTokenService>();
 builder.Services.AddScoped<IAuthService, AuthService>();
 builder.Services.AddScoped<ITaskMapper, TaskMapper>();
 builder.Services.AddScoped<ITaskService, TaskService>();
 
-// Configure JWT authentication
 var key = Encoding.UTF8.GetBytes(builder.Configuration["Jwt:Secret"]!);
+
 builder.Services.AddAuthentication(options =>
 {
     options.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
@@ -47,7 +41,6 @@ builder.Services.AddAuthentication(options =>
     };
 });
 
-// Configure controllers
 builder.Services.AddControllers()
     .AddJsonOptions(options =>
     {
@@ -55,20 +48,11 @@ builder.Services.AddControllers()
         options.JsonSerializerOptions.ReferenceHandler = System.Text.Json.Serialization.ReferenceHandler.IgnoreCycles;
     });
 
-// Configure Swagger
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
 
-// Create database and tables if they don't exist
-using (var scope = app.Services.CreateScope())
-{
-    var dbContext = scope.ServiceProvider.GetRequiredService<ApplicationDbContext>();
-    dbContext.Database.EnsureCreated();
-}
-
-// Configure the HTTP request pipeline
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
